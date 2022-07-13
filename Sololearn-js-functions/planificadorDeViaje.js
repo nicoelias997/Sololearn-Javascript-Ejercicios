291766996ac2d484358a4c9e532487197ed4cdb1
nicoelias997 <nicoelias997@gmail.com>
2022-07-13 18:46:43 +0200

    Termine el ejercicio, referido a conceptos basicos de js

---
+++ Sololearn-js-functions/planificadorDeViaje.js
@@ -0,0 +1,13 @@
+// Planificador de viajes
+// Necesitas planear un viaje por carretera. Estás viajando a una velocidad promedio de 40 millas por hora .
+// Dada una distancia en millas como entrada (el código para tomar la entrada ya está presente), envíe a la consola el tiempo que le llevará recorrerla en minutos . Entrada de muestra: 150 Salida de muestra: 225
+function main() {
+    var distance = parseInt(readLine(), 10);
+    
+    
+}
+function main(distance){
+    const velocidad = 40;
+    return distance/velocidad*60;
+}
+console.log(main(100));
